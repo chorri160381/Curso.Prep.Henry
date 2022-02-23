27abe2986465fd056dfeb46d2b187cf24e3cc2c1
Chorri160381 <edytorres2009@hotmail.com>
2022-02-23 12:07:45 -0500

    archivos modificados 02-JS-I y 03-JS-II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -89,7 +89,7 @@ function mayorQueCincuenta(num){
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num < 50){
+  if(num > 50){
     return true;
 }else{
   return false;
